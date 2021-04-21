83c28254328557644fad58bed399fbf20f1fc316
kraftkinder <innovation.haus1@gmail.com>
2021-04-21 12:47:31 +0200

    adding 3 animations red, gree and blue color ramps
    The Button works only sometimes?! 🤔

--- LED_test_Pushbutton_156px/LED_test_Pushbutton_156px.ino
+++ LED_test_Pushbutton_156px/LED_test_Pushbutton_156px.ino
@@ -4,6 +4,9 @@
 // The LED Animations
 #include "a_red.h"
 #include "a_blue.h"
+#include "ramp_r.h"
+#include "ramp_g.h"
+#include "ramp_b.h"
 
 // Variables
 const int numled = 156;
@@ -25,7 +28,9 @@ WS2812Serial leds(numled, displayMemory, drawingMemory, pin, WS2812_GRB);
 
 const int animationSize_red = sizeof animationData_red / sizeof animationData_red[0];
 const int animationSize_blue = sizeof animationData_blue / sizeof animationData_blue[0];
-
+const int animationSize_ramp_r = sizeof animationData_ramp_r / sizeof animationData_ramp_r[0];
+const int animationSize_ramp_g = sizeof animationData_ramp_g / sizeof animationData_ramp_g[0];
+const int animationSize_ramp_b = sizeof animationData_ramp_b / sizeof animationData_ramp_b[0];
 
 void setup() {
   leds.begin();
@@ -66,48 +71,48 @@ void loop() {
   // a Pushbutton toggles following the cases
   switch (btnCounter) {
     case 1 :
-      for (int k = 0; k < animationSize_red / (3 * leds.numPixels()); k++) {
+      for (int k = 0; k < animationSize_ramp_r / (3 * leds.numPixels()); k++) {
         for (int i = 0; i < leds.numPixels() - 1; i++) {
-          uint8_t red = animationData_red[3 * i + (k * leds.numPixels() * 3) + 0];
-          uint8_t green = animationData_red[3 * i + (k * leds.numPixels() * 3) + 1];
-          uint8_t blue = animationData_red[3 * i + (k * leds.numPixels() * 3) + 2];
+          uint8_t red = animationData_ramp_r[3 * i + (k * leds.numPixels() * 3) + 0];
+          uint8_t green = animationData_ramp_r[3 * i + (k * leds.numPixels() * 3) + 1];
+          uint8_t blue = animationData_ramp_r[3 * i + (k * leds.numPixels() * 3) + 2];
           leds.setPixel(i, leds.Color(red, green, blue));
         }
         int poti = analogRead(A17);
         int bright_poti = poti / 4;
         leds.setBrightness(bright_poti);
         leds.show();
-        delay(1); // 33 ms delay, 1/0.033 sind ungefähr 30 fps
+        delay(15); // 33 ms delay, 1/0.033 sind ungefähr 30 fps
       }
       break;
     case 2:
-      for (int k = 0; k < animationSize_blue / (3 * leds.numPixels()); k++) {
+      for (int k = 0; k < animationSize_ramp_g / (3 * leds.numPixels()); k++) {
         for (int i = 0; i < leds.numPixels() - 1; i++) {
-          uint8_t red = animationData_blue[3 * i + (k * leds.numPixels() * 3) + 0];
-          uint8_t green = animationData_blue[3 * i + (k * leds.numPixels() * 3) + 1];
-          uint8_t blue = animationData_blue[3 * i + (k * leds.numPixels() * 3) + 2];
+          uint8_t red = animationData_ramp_g[3 * i + (k * leds.numPixels() * 3) + 0];
+          uint8_t green = animationData_ramp_g[3 * i + (k * leds.numPixels() * 3) + 1];
+          uint8_t blue = animationData_ramp_g[3 * i + (k * leds.numPixels() * 3) + 2];
           leds.setPixel(i, leds.Color(red, green, blue));
         }
         int poti = analogRead(A17);
         int bright_poti = poti / 4;
         leds.setBrightness(bright_poti);
         leds.show();
-        delay(1); // 33 ms delay, 1/0.033 sind ungefähr 30 fps
+        delay(15); // 33 ms delay, 1/0.033 sind ungefähr 30 fps
       }
       break;
     default:
-      for (int k = 0; k < animationSize_blue / (3 * leds.numPixels()); k++) {
+         for (int k = 0; k < animationSize_ramp_b / (3 * leds.numPixels()); k++) {
         for (int i = 0; i < leds.numPixels() - 1; i++) {
-          uint8_t red = animationData_blue[3 * i + (k * leds.numPixels() * 3) + 0];
-          uint8_t green = animationData_blue[3 * i + (k * leds.numPixels() * 3) + 1];
-          uint8_t blue = animationData_blue[3 * i + (k * leds.numPixels() * 3) + 2];
+          uint8_t red = animationData_ramp_b[3 * i + (k * leds.numPixels() * 3) + 0];
+          uint8_t green = animationData_ramp_b[3 * i + (k * leds.numPixels() * 3) + 1];
+          uint8_t blue = animationData_ramp_b[3 * i + (k * leds.numPixels() * 3) + 2];
           leds.setPixel(i, leds.Color(red, green, blue));
         }
         int poti = analogRead(A17);
         int bright_poti = poti / 4;
         leds.setBrightness(bright_poti);
         leds.show();
-        delay(1); // 33 ms delay, 1/0.033 sind ungefähr 30 fps
+        delay(15); // 33 ms delay, 1/0.033 sind ungefähr 30 fps
       }
       btnCounter = 0;
   }
